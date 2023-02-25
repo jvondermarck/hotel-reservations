117210cfb1cc02bb2d80d96eea1d665d307781b1
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-02-25 12:29:10 +0100

    feat: add a view blade file to display the getting started API Connector page

--- resources/views/getting-started.blade.php
+++ resources/views/getting-started.blade.php
@@ -1,9 +1,14 @@
-<h1>Getting Started</h1>
+@extends('layouts.app')
 
-<ul>
-    <li>
-        <h2>{{ $data['Enterprise']['Name'] }}</h2>
-        <p>{{ $data['Enterprise']['Address']['Line1'] }}, {{ $data['Enterprise']['Address']['City'] }}, {{ $data['Enterprise']['Address']['CountryCode'] }}</p>
-        <p>Phone: {{ $data['Enterprise']['Phone'] }}, Email: {{ $data['Enterprise']['Email'] }}</p>
-    </li>
-</ul>
+@section('title','Getting Started')
+
+@section('main')
+    @parent
+    <ul>
+        <li>
+            <h2>{{ $data['Enterprise']['Name'] }}</h2>
+            <p>{{ $data['Enterprise']['Address']['Line1'] }}, {{ $data['Enterprise']['Address']['City'] }}, {{ $data['Enterprise']['Address']['CountryCode'] }}</p>
+            <p>Phone: {{ $data['Enterprise']['Phone'] }}, Email: {{ $data['Enterprise']['Email'] }}</p>
+        </li>
+    </ul>
+@endsection
